e3045b67a4174d1ecbd205897a8b7a8a3eda528f
Alexandra Iordache <aghecen@amazon.com>
2020-09-08 15:43:49 +0300

    aarch64: Fix build
    Signed-off-by: Alexandra Iordache <aghecen@amazon.com>

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,33 @@
 // Copyright 2020 Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0 OR BSD-3-Clause
 
-#![cfg(target_arch = "x86_64")]
-
 extern crate cli;
 extern crate vmm;
 
+#[cfg(target_arch = "x86_64")]
 use std::convert::TryFrom;
+#[cfg(target_arch = "x86_64")]
 use std::env;
 
+#[cfg(target_arch = "x86_64")]
 use cli::CLI;
+#[cfg(target_arch = "x86_64")]
 use vmm::VMM;
 
 fn main() {
-    let vmm_config = CLI::launch(
-        env::args()
-            .collect::<Vec<String>>()
-            .iter()
-            .map(|s| s.as_str())
-            .collect(),
-    )
-    .expect("Failed to parse command line options");
-    let mut vmm = VMM::try_from(vmm_config).expect("Failed to create VMM from configurations");
-    vmm.run();
+    #[cfg(target_arch = "x86_64")]
+    {
+        let vmm_config = CLI::launch(
+            env::args()
+                .collect::<Vec<String>>()
+                .iter()
+                .map(|s| s.as_str())
+                .collect(),
+        )
+        .expect("Failed to parse command line options");
+        let mut vmm = VMM::try_from(vmm_config).expect("Failed to create VMM from configurations");
+        vmm.run();
+    }
+    #[cfg(target_arch = "aarch64")]
+    println!("Reference VMM under construction!")
 }
